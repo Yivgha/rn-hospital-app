ec1218590a14f876d8a8e8f09a28bf17270f44e4
unknown <genieukne@gmail.com>
2024-04-22 21:38:45 +0300

    fix slider animation

--- App/Components/Home/Slider.js
+++ App/Components/Home/Slider.js
@@ -1,19 +1,21 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import {
   View,
   StyleSheet,
-  FlatList,
   Image,
   Dimensions,
   Animated,
+  Easing,
 } from "react-native";
 import GlobalApi from "../../Services/GlobalApi";
 import { SubHeading } from "./SubHeading";
 
-const ITEM_WIDTH = Dimensions.get("screen").width * 0.8;
+const ITEM_WIDTH = Dimensions.get("screen").width * 0.9;
 
 export function Slider() {
   const [sliderData, setSliderData] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const slideAnim = new Animated.Value(-currentIndex * ITEM_WIDTH);
 
   useEffect(() => {
     fetchSlider();
@@ -26,23 +28,48 @@ export function Slider() {
   if (!sliderData) {
     return null;
   }
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === sliderData.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  useEffect(() => {
+    const interval = setInterval(nextSlide, 3000);
+    return () => clearInterval(interval);
+  }, [currentIndex]);
+
+  const slideAnimation = () => {
+    Animated.timing(slideAnim, {
+      toValue: -currentIndex * ITEM_WIDTH,
+      duration: 500,
+      easing: Easing.ease,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  useEffect(() => {
+    slideAnimation();
+  }, [currentIndex]);
 
   return (
     <View style={styles.sliderBox}>
-      <SubHeading subHeading={"Slide to the right to see more"} />
-      <FlatList
-        data={sliderData}
-        horizontal={true}
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={{ gap: 5 }}
-        renderItem={({ item, index }) => (
+      <SubHeading subHeading={"See what's new"} />
+      <Animated.View
+        style={[
+          styles.slideWrapper,
+          { transform: [{ translateX: slideAnim }] },
+        ]}
+      >
+        {sliderData.map((image, index) => (
           <Image
-            source={{ uri: item?.attributes.Image.data.attributes.url }}
-            alt={item.name}
+            key={index}
+            source={{ uri: image?.attributes.Image.data.attributes.url }}
             style={styles.sliderImg}
+            resizeMode="cover"
           />
-        )}
-      />
+        ))}
+      </Animated.View>
     </View>
   );
 }
@@ -51,9 +78,17 @@ const styles = StyleSheet.create({
   sliderBox: {
     flexDirection: "column",
     gap: 7,
+    overflow: "hidden",
+    width: ITEM_WIDTH,
+    height: 210,
+    borderRadius: 10,
+  },
+  slideWrapper: {
+    flexDirection: "row",
+    width: ITEM_WIDTH,
   },
   sliderImg: {
-    height: 170,
+    height: 210,
     width: ITEM_WIDTH,
     borderRadius: 10,
   },
